f0700af1bf4076a12e2aeabcc20daf69a486a8d4
lumarin2802 <marinperezluciana@gmail.com>
2023-01-11 02:55:23 +0000

    la logica esta planteada, faltan los estilos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue ] = useState("");
+  const [todos, setTodos ] = useState([]);
+  
+  return (
+    
+    <div className="container d-flex justify-content-center mt-100"> 
+    
+      <div className="card d-flex " style={{width: "18rem"}}>
+      <div className="card-header text-center text-secondary w-100 p-4 ">
+    To Do List
+  </div> 
+        <ul className="list-group list-group-flush mb-0 p-0 border border-secondary">
+          <li className="list-group-item p-3 bg-light border border-secondary" >
+          <input className="w-100 p-2" type="text"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        onKeyPress={(e) => {
+          //console.log("key was pressed", e.key)
+          if(e.key === "Enter") { 
+            //console.log("enter was pressed");
+             setTodos(todos.concat([inputValue]));
+             setInputValue("");
+            }
+          }}
+         placeholder="What do you need to do?"></input>
+      </li>
+      {todos.map((item, index) => (
+      <li>
+      {item}{""} 
+      <i class="fas fa-trash-alt" 
+      onClick={() => 
+        setTodos(
+        todos.filter(
+          (t, currentIndex) => 
+          index != currentIndex
+          )
+          )
+          
+          }></i>
+     </li>
+     ))}
+             
+          
+        </ul>
+        <div> {todos.length}</div>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Home;
